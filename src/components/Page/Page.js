4999d96ded6d59a9b6798eebeadd03f6fe1b89a6
g-crow <genevieve.crow@gmail.com>
2020-04-29 11:18:00 -0600

    Add form to contact page

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -11,14 +11,31 @@ const Page = ({ title, children }: Props) => {
 
   useEffect(() => {
     pageRef.current.scrollIntoView();
+    console.log(title)
   });
 
   return (
     <div ref={pageRef} className={styles['page']}>
-      <div className={styles['page__inner']}>
-        { title && <h1 className={styles['page__title']}>{title}</h1>}
-        <div className={styles['page__body']}>
+        <div className={styles['page__inner']}>
+          { title && <h1 className={styles['page__title']}>{title}</h1>}
+          <div className={styles['page__body']}>
           {children}
+          {title === "Contact me" && 
+            <form name="contact" method="POST" data-netlify="true">
+            <p>
+              <label>Your Name: <input type="text" name="name" /></label>   
+            </p>
+            <p>
+              <label>Your Email: <input type="email" name="email" /></label>
+            </p>
+            <p>
+              <label>Message: <textarea name="message"></textarea></label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
+          }
         </div>
       </div>
     </div>
